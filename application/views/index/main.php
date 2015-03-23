cde208d99f6ed7ad8b772db9fcfb4a8bd38417e1
fabkxd <847525974@qq.com>
2015-03-23 12:45:33 +0800

    index页面修改

---
+++ application/views/index/main.php
@@ -0,0 +1,231 @@
+<div class="row" style="margin-top: 20px;">
+ <div class=" leftpart">
+  
+   <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+    </ol>
+    
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active"><img src="<?=$baseurl?>img/bg/1.jpg" alt=""/></div>
+      <div class="item"><img src="<?=$baseurl?>img/bg/2.jpg" alt=""/></div>
+      <div class="item"><img src="<?=$baseurl?>img/bg/3.jpg" alt=""/></div>
+      <div class="item"><img src="<?=$baseurl?>img/bg/4.jpg" alt=""/></div>
+    </div>
+    
+    <!-- Controls --> 
+    <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev"> 
+     <span class="glyphicon glyphicon-chevron-left"></span> 
+   </a>
+   <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+     <span class="glyphicon glyphicon-chevron-right"></span> 
+   </a> 
+ </div>
+ <div class="lefttitle">自动匹配 | 本站
+  <a class=" text-muted switch btn-link">切换到绑定BBS</a>
+  <a class=" text-primary all pull-right" href="#">全部</a>
+</div>
+
+<div class="leftrow">
+  <div class="postitembig">
+    <img class="postimage" src="<?=$baseurl?>img/jinxismall.jpg" alt=""/>
+    
+    <div class="postinfo">
+      <a href="#" class="posttitle text-primary">【求购】手机：诺基亚C6</a>
+      <p class="postaddress">北京航天航空大学</p>
+      <p class="postuser">
+        <span class=" fui-user"><a href="#" class=" text-info">罗天航</a></span>
+        <span class=" fui-time" style=" margin-left: 8px;">2013-11-19 20：00：00</span>
+        <span class=" fui-new" style=" margin-left: 8px;">12</span>
+        <span class=" fui-eye" style=" margin-left: 8px;">33</span>
+      </p>
+    </div>
+    
+    <div class="postquality">
+      <img class="quality" src="<?=$baseurl?>img/class/2.png" alt=""/>
+      <p class="postprice">￥800</p>
+    </div>
+  </div>
+</div>
+
+<div class="leftrow">
+  <div style=" position: relative; float: left; width: 350px;">
+    <a href="#" class="posttitlesm text-primary">【求购】手机：诺基亚C6</a>
+  </div>
+  <div style=" position: relative; float: left; width: 422px;">
+    <p class="postusersm pull-right">
+      <span class=" fui-user"><a href="#" class=" text-info">罗天航</a></span>
+      <span class=" fui-time" style=" margin-left: 8px;">2013-11-19 20：00：00</span>
+    </p>
+  </div>
+  
+  <div style=" position: relative; float: left; width: 350px;">
+    <a href="#" class="posttitlesm text-primary">【求购】手机：诺基亚C6</a>
+  </div>
+  <div style=" position: relative; float: left; width: 422px;">
+    <p class="postusersm pull-right">
+      <span class=" fui-user"><a href="#" class=" text-info">罗天航</a></span>
+      <span class=" fui-time" style=" margin-left: 8px;">2013-11-19 20：00：00</span>
+    </p>
+  </div>
+  
+  <div style=" position: relative; float: left; width: 350px;">
+    <a href="#" class="posttitlesm text-primary">【求购】手机：诺基亚C6</a>
+  </div>
+  <div style=" position: relative; float: left; width: 422px;">
+    <p class="postusersm pull-right">
+      <span class=" fui-user"><a href="#" class=" text-info">罗天航</a></span>
+      <span class=" fui-time" style=" margin-left: 8px;">2013-11-19 20：00：00</span>
+    </p>
+  </div>
+  
+  <div style=" position: relative; float: left; width: 350px;">
+    <a href="#" class="posttitlesm text-primary">【求购】手机：诺基亚C6</a>
+  </div>
+  <div style=" position: relative; float: left; width: 422px;">
+    <p class="postusersm pull-right">
+      <span class=" fui-user"><a href="#" class=" text-info">罗天航</a></span>
+      <span class=" fui-time" style=" margin-left: 8px;">2013-11-19 20：00：00</span>
+    </p>
+  </div>
+</div>
+
+<div class="lefttitle">
+  全站推荐 | 全站 <a class=" text-muted switch btn-link">切换到本校</a> <a
+  class=" text-primary all pull-right" href="#">全部</a>
+</div>
+
+<div class="leftrow">
+  <div class="postitembig">
+    <img class="postimage" src="<?=$baseurl?>img/jinxismall.jpg" alt="" />
+
+    <div class="postinfo">
+      <a href="#" class="posttitle text-primary">【求购】手机：诺基亚C6</a>
+      <p class="postaddress">北京航天航空大学</p>
+      <p class="postuser">
+        <span class=" fui-user"><a href="#" class=" text-info">罗天航</a></span>
+        <span class=" fui-time" style="margin-left: 8px;">2013-11-19
+          20：00：00</span> <span class=" fui-new" style="margin-left: 8px;">12</span>
+          <span class=" fui-eye" style="margin-left: 8px;">33</span>
+        </p>
+      </div>
+
+      <div class="postquality">
+        <img class="quality" src="<?=$baseurl?>img/class/2.png" alt="" />
+        <p class="postprice">￥500</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="leftrow">
+    <div style="position: relative; float: left; width: 350px;">
+      <a href="#" class="posttitlesm text-primary">【求购】手机：诺基亚C6</a>
+    </div>
+    <div style="position: relative; float: left; width: 422px;">
+      <p class="postusersm pull-right">
+        <span class=" fui-user"><a href="#" class=" text-info">罗天航</a></span>
+        <span class=" fui-time" style="margin-left: 8px;">2013-11-19 20：00：00</span>
+      </p>
+    </div>
+
+    <div style="position: relative; float: left; width: 350px;">
+      <a href="#" class="posttitlesm text-primary">【求购】手机：诺基亚C6</a>
+    </div>
+    <div style="position: relative; float: left; width: 422px;">
+      <p class="postusersm pull-right">
+        <span class=" fui-user"><a href="#" class=" text-info">罗天航</a></span>
+        <span class=" fui-time" style="margin-left: 8px;">2013-11-19 20：00：00</span>
+      </p>
+    </div>
+
+    <div style="position: relative; float: left; width: 350px;">
+      <a href="#" class="posttitlesm text-primary">【求购】手机：诺基亚C6</a>
+    </div>
+    <div style="position: relative; float: left; width: 422px;">
+      <p class="postusersm pull-right">
+        <span class=" fui-user"><a href="#" class=" text-info">罗天航</a></span>
+        <span class=" fui-time" style="margin-left: 8px;">2013-11-19 20：00：00</span>
+      </p>
+    </div>
+
+    <div style="position: relative; float: left; width: 350px;">
+      <a href="#" class="posttitlesm text-primary">【求购】手机：诺基亚C6</a>
+    </div>
+    <div style="position: relative; float: left; width: 422px;">
+      <p class="postusersm pull-right">
+        <span class=" fui-user"><a href="#" class=" text-info">罗天航</a></span>
+        <span class=" fui-time" style="margin-left: 8px;">2013-11-19 20：00：00</span>
+      </p>
+    </div>
+  </div>
+
+
+  <div class="lefttitle">
+    教材专区 | 全站 <a class=" text-muted switch btn-link">切换到本校</a> <a
+    class=" text-primary all pull-right" href="#">全部</a>
+  </div>
+  <div class="leftrow"
+  style="width: 802px; margin-left: -15px; margin-top: 15px;">
+  <div class="textbookitem">
+    <img src="<?=$baseurl?>img/textbook/gslc.jpg" alt="" style="width: 100%" />
+    <p class="tbtitle">公司理财（罗斯著）</p>
+    <p class="tbtitle">机械工业出版社</p>
+    <p class="tbinfo">已有119人求购，300人转让</p>
+  </div>
+  <div class="textbookitem">
+    <img src="<?=$baseurl?>img/textbook/gslc.jpg" alt="" style="width: 100%" />
+    <p class="tbtitle">公司理财（罗斯著）</p>
+    <p class="tbtitle">机械工业出版社</p>
+    <p class="tbinfo">已有119人求购，300人转让</p>
+  </div>
+  <div class="textbookitem">
+    <img src="<?=$baseurl?>img/textbook/gslc.jpg" alt="" style="width: 100%" />
+    <p class="tbtitle">公司理财（罗斯著）</p>
+    <p class="tbtitle">机械工业出版社</p>
+    <p class="tbinfo">已有119人求购，300人转让</p>
+  </div>
+  <div class="textbookitem">
+    <img src="<?=$baseurl?>img/textbook/gslc.jpg" alt="" style="width: 100%" />
+    <p class="tbtitle">公司理财（罗斯著）</p>
+    <p class="tbtitle">机械工业出版社</p>
+    <p class="tbinfo">已有119人求购，300人转让</p>
+  </div>
+</div>
+
+
+<div class="leftrow" style="margin-top: 15px;">
+  <div class="panel panel-default"
+  style="width: 378px; padding: 20px; position: relative; float: left;">
+  常见问题<a class="text-info btn-link pull-right"
+  style="font-size: 13px; font-family: 宋体;">查看全部</a>
+  <ul>
+    <li><a href="#" class="text-info" style="font-size: 14px;">常见问题</a></li>
+    <li><a href="#" class="text-info" style="font-size: 14px;">常见问题</a></li>
+    <li><a href="#" class="text-info" style="font-size: 14px;">常见问题</a></li>
+    <li><a href="#" class="text-info" style="font-size: 14px;">常见问题</a></li>
+    <li><a href="#" class="text-info" style="font-size: 14px;">常见问题</a></li>
+  </ul>
+</div>
+<div class="panel panel-default"
+style="margin-left: 16px; width: 378px; padding: 20px; position: relative; float: left;">
+相关资讯
+<ul>
+  <li><a href="#" class="text-info" style="font-size: 14px;">相关资讯</a></li>
+  <li><a href="#" class="text-info" style="font-size: 14px;">相关资讯</a></li>
+  <li><a href="#" class="text-info" style="font-size: 14px;">相关资讯</a></li>
+  <li><a href="#" class="text-info" style="font-size: 14px;">相关资讯</a></li>
+  <li><a href="#" class="text-info" style="font-size: 14px;">相关资讯</a></li>
+</ul>
+</div>
+</div>
+<div class="leftrow">
+  <!-- UJian Button BEGIN -->
+  <div class="ujian-hook"></div>
+  <!-- UJian Button END -->
+</div>
+</div>
