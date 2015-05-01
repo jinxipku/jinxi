43eb3f1fd0d43b99e4c241d50438c8e3fb88a7e6
fabkxd <847525974@qq.com>
2015-05-01 19:58:38 +0800

    help

--- application/controllers/info.php
+++ application/controllers/info.php
@@ -49,6 +49,11 @@ class Info extends MY_Controller {
 	}
 
 	public function about(){
+		$login_user =  $this->session->userdata('login_user');
+		if (!empty($login_user)) {
+			$this->assign('login_user', $login_user);
+		}
+
 		$this->assign('nav_tab', 4);
 		$this->assign('title', '今昔网-关于');
 		$this->assign('baseurl', base_url());
@@ -68,12 +73,36 @@ class Info extends MY_Controller {
 		$this->display ( 'templates/footer.php' );
 	}
 
+	public function help(){
+		$login_user =  $this->session->userdata('login_user');
+		if (!empty($login_user)) {
+			$this->assign('login_user', $login_user);
+		}
+
+		$this->assign('nav_tab', 5);
+		$this->assign('title', '今昔网-帮助');
+		$this->assign('baseurl', base_url());
+		$this->assign('tips', show_tips());
+
+		$hotest = $this->post_model->get_hotest_post($login_user['id']);
+		$newest = $this->post_model->get_newest_post($login_user['id']);
+		$random = $this->post_model->get_random_post();
+		$this->assign('hotest', $hotest);
+		$this->assign('newest', $newest);
+		$this->assign('random', $random);
+
+		$this->display ( 'templates/header.php' );
+		$this->display ( 'info/help.php' );
+		$this->display ( 'templates/side.php' );
+		$this->display ( 'templates/footer.php' );
+	}
+
 	public function agreement(){
 		$login_user =  $this->session->userdata('login_user');
 		if (!empty($login_user)) {
 			$this->assign('login_user', $login_user);
 		}
-		
+
 		$this->assign('nav_tab', 0);
 		$this->assign('title', '今昔网-协议');
 		$this->assign('baseurl', base_url());
---
+++ application/views/info/help.php
@@ -0,0 +1,30 @@
+	<div id="body" class="row">
+		<div id="main" class="agreement">
+			<h6><strong>今昔网 帮助中心</strong></h6>
+			<hr/>
+			
+			<br/>
+			<blockquote><strong>今昔网四大特色</strong></blockquote>
+			<p>
+				1、精心制作的网页UI<br/>
+    			2、系统自动匹配需求<br/>
+    			3、手机扫码上传照片<br/>
+    			4、买卖分离，分类筛选<br/>
+    		</p>
+
+    		<blockquote><strong>我们是谁？</strong></blockquote>
+			<p>
+				我们是来自海淀区若干所高校的烟酒僧，是一个致力于服务大众的高校团队，您的支持是我们不断改进的动力，我们将竭诚为您服务！
+    		</p>
+
+    		<blockquote><strong>为什么叫今昔？</strong></blockquote>
+			<p>
+				今昔意寓昔日的物品今天依然有价值！所以，小伙伴们快把你们的闲置物品都放上来吧~
+    		</p>
+
+    		<blockquote><strong>物品品质是什么？</strong></blockquote>
+			<p>
+				今昔网二手物品的四种品质S、A、B、C描述了物品的新旧程度、外观及功能现状。
+    		</p>
+    				
+		</div>
