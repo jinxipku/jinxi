afaa05e6a54b4be300659a7924a52dcb16c221c6
fabkxd <847525974@qq.com>
2015-05-21 15:10:42 +0800

    翻页bug

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -108,8 +108,8 @@ class User extends MY_Controller {
 		$this->assign('posts', $data['posts']);
 
 		if ($data['page_num'] > 0) {
-			$this->assign('st_page', floor($data['cur_page'] / 10) * 10 + 1);
-			$end_page = floor($data['cur_page'] / 10 + 1) * 10;
+			$this->assign('st_page', floor(($data['cur_page'] - 1) / 10) * 10 + 1);
+			$end_page = floor(($data['cur_page'] - 1) / 10 + 1) * 10;
 			if ($end_page > $data['page_num'])
 				$end_page = $data['page_num'];
 			$this->assign('ed_page', $end_page);
